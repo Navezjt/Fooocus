7345462c75672e260a6e6152d02004f5508aa2b6
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-09-10 13:28:42 -0500

    Add files via upload

---
+++ modules/expansion.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, set_seed
+from modules.path import fooocus_expansion_path
+
+
+def safe_str(x):
+    for _ in range(16):
+        x = x.replace('  ', ' ')
+    return str(x).rstrip(",. \r\n")
+
+
+class FooocusExpansion:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(fooocus_expansion_path)
+        self.model = AutoModelForCausalLM.from_pretrained(fooocus_expansion_path)
+        self.pipe = pipeline('text-generation',
+                             model=self.model,
+                             tokenizer=self.tokenizer,
+                             device='cpu',
+                             torch_dtype=torch.float32)
+        print('Fooocus Expansion engine loaded.')
+
+    def __call__(self, prompt, seed):
+        prompt = safe_str(prompt) + ', '  # Reduce semantic corruption.
+        seed = int(seed)
+        set_seed(seed)
+        response = self.pipe(prompt, max_length=len(prompt) + 256)
+        result = response[0]['generated_text']
+        result = safe_str(result)
+        return result
